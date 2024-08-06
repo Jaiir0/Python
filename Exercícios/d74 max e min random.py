912eb3da6009d78d5f4e6ff97c3a66cadd44168b
Jairo <164819215+Jaiir0@users.noreply.github.com>
2024-08-06 00:49:41 -0300

    Add files via upload

---
+++ Exercícios/d74 max e min random.py
@@ -0,0 +1,9 @@
+from random import randint
+tupla = ()
+
+for c in range (0,5):
+    sorteio = randint(1,9),
+    tupla += sorteio
+
+print (tupla)
+print (f'O maior número é: {max (tupla)} e o menor é: {min(tupla)} ')
