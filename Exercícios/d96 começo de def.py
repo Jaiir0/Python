d84c9abbaca8a01899f27b22048f733c7a3f8dbd
Jairo <164819215+Jaiir0@users.noreply.github.com>
2024-08-06 01:21:30 -0300

    Add files via upload

---
+++ Exercícios/d96 começo de def.py
@@ -0,0 +1,10 @@
+def area(a, b):
+    s = a * b
+    print(f'a area foi de {s}')
+
+
+comprimento = float(input('comprimento: '))
+largura = float(input('largura: '))
+
+area(comprimento, largura)
+
